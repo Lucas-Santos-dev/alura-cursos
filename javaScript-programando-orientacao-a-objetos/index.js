b974189030903cb140383014fb16f06a2f8447e3
Lucas-Santos-dev <lucassantosrocha889@gmail.com>
2025-06-26 00:24:49 -0300

    updade second class

--- javaScript-programando-orientacao-a-objetos/index.js
+++ javaScript-programando-orientacao-a-objetos/index.js
@@ -10,30 +10,31 @@ class ContaCorrente {
     sacar(valor){
         if (this.saldo >= valor) {
             this.saldo -= valor;
+            console.log(`Saque no valor de R$ ${valor} realizado com sucesso.`)
+            console.log(`Saldo atual: R$ ${this.saldo}`);
+        }else {
+            console.log(`Saldo insuficiente para saque de R$ ${valor}. Saldo atual: R$ ${this.saldo}`);
         }
-    };
+    }
+
+    depositar(valor) {
+        if (valor > 0) {
+            this.saldo += valor;
+            console.log(`Deposito no valor de R$ ${valor} realizado com sucesso.`)
+            console.log(`Saldo atual: R$ ${this.saldo}`);
+        }
+    }
+
 }
 
 const customer = new Customer();
 customer.name = "Lucas";
 customer.cpf = 9999999999;
 
-const customer1 = new Customer();
-customer1.name = "Rayssa";
-customer1.cpf = 88888888888;
-
 const contaCorrenteLucas = new ContaCorrente();
 contaCorrenteLucas.saldo = 0;
 contaCorrenteLucas.agency = 1001;
-contaCorrenteLucas.saldo = 100;
-contaCorrenteLucas.sacar(50);
+contaCorrenteLucas.depositar(50);
 
-console.log(contaCorrenteLucas.saldo)
-contaCorrenteLucas.saldo = 100;
-console.log(contaCorrenteLucas.saldo)
-contaCorrenteLucas.sacar(10);
-
+console.log(contaCorrenteLucas);
 console.log(contaCorrenteLucas.saldo);
-
-console.log(customer);
-console.log(customer1);
