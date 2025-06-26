a2f923ef2fb3e8180318ea30a05de7397b8e1918
Lucas-Santos-dev <lucassantosrocha889@gmail.com>
2025-06-25 23:51:19 -0300

    segunda aula

--- javaScript-programando-orientacao-a-objetos/index.js
+++ javaScript-programando-orientacao-a-objetos/index.js
@@ -1,21 +1,39 @@
 class Customer {
     name;
     cpf;
+}
+
+class ContaCorrente {
     agency;
     balance;
+
+    sacar(valor){
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+        }
+    };
 }
 
 const customer = new Customer();
-const customer1 = new Customer();
-
 customer.name = "Lucas";
 customer.cpf = 9999999999;
-customer.agency = 1001;
-customer.balance = 0;
 
+const customer1 = new Customer();
 customer1.name = "Rayssa";
 customer1.cpf = 88888888888;
-customer1.agency = 1001;
-customer1.balance = 0;
 
-console.log(customer, customer1);
+const contaCorrenteLucas = new ContaCorrente();
+contaCorrenteLucas.saldo = 0;
+contaCorrenteLucas.agency = 1001;
+contaCorrenteLucas.saldo = 100;
+contaCorrenteLucas.sacar(50);
+
+console.log(contaCorrenteLucas.saldo)
+contaCorrenteLucas.saldo = 100;
+console.log(contaCorrenteLucas.saldo)
+contaCorrenteLucas.sacar(10);
+
+console.log(contaCorrenteLucas.saldo);
+
+console.log(customer);
+console.log(customer1);
