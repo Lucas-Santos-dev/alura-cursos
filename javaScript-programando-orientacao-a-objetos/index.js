1d0b3afdfdb529f7ed948a7f91388d9220aa2950
Lucas Santos <lucassantosrocha889@gmail.com>
2025-07-02 23:54:55 -0300

    Aula 3

--- javaScript-programando-orientacao-a-objetos/index.js
+++ javaScript-programando-orientacao-a-objetos/index.js
@@ -27,8 +27,17 @@ account2.customer.name = "Davi";
 account2.customer.cpf = 77777777777;
 account2.agency = 1003;
 
+const account3 = new CurrentAccount();
+account3.customer = new Customer;
+account3.name = "Luana";
+account3.cpf = 66666666666;
+account3.agency = 1004;
+
+account0.transfer(200, account3);
+
 
 console.log(account0);
 console.log(account1);
 console.log(account2);
+console.log(account3);
 
