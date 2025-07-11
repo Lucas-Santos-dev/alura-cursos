53632a80619ef1d8d2aeae509279518fc2187991
Lucas-Santos-dev <lucassantosrocha889@gmail.com>
2025-07-11 00:26:28 -0300

    Class 3

---
+++ typeScript-part-1-evolving-your-javaScript/app/controllers/negotiation-controller.ts
@@ -0,0 +1,23 @@
+import { Negotiation } from "../models/negotiation.js";
+
+export class NegotiationController {
+    private inputDate;
+    private inputAmount;
+    private inputValue;
+
+    constructor() {
+        this.inputDate = document.querySelector('#date');
+        this.inputAmount = document.querySelector('#amount');
+        this.inputValue = document.querySelector("#value")
+    }
+
+    add() {
+       const negotiation = new Negotiation(
+        this.inputDate.value,
+        this.inputAmount.value,
+        this.inputValue.value
+       );
+
+       console.log(negotiation);
+    }
+}
---
+++ typeScript-part-1-evolving-your-javaScript/dist/js/controllers/negotiation-controller.js
@@ -0,0 +1,12 @@
+import { Negotiation } from "../models/negotiation.js";
+export class NegotiationController {
+    constructor() {
+        this.inputDate = document.querySelector('#date');
+        this.inputAmount = document.querySelector('#amount');
+        this.inputValue = document.querySelector("#value");
+    }
+    add() {
+        const negotiation = new Negotiation(this.inputDate.value, this.inputAmount.value, this.inputValue.value);
+        console.log(negotiation);
+    }
+}
