bbd5ef5bb8e08b260339af6787da83cea06370c8
Lucas-Santos-dev <lucassantosrocha889@gmail.com>
2025-07-10 23:49:39 -0300

    Class 2

--- typeScript-part-1-evolving-your-javaScript/app/models/negotiation.ts
+++ typeScript-part-1-evolving-your-javaScript/app/models/negotiation.ts
@@ -1,20 +1,20 @@
 export class Negotiation {
-    #date;
-    #amount;
-    #value;
+    private _date;
+    private _amount;
+    private _value;
 
     constructor(date, amount, value){
-        this.#date = date;
-        this.#amount = amount;
-        this.#value = value;
+        this._date = date;
+        this._amount = amount;
+        this._value = value;
     }
 
     get date(){
-        return this.#date;
+        return this._date;
     }
 
     get amount(){
-        return this.#amount;
+        return this._amount;
     }
 
     get value(){
@@ -22,6 +22,6 @@ export class Negotiation {
     }
 
     get volume(){
-        return this.#amount * this.#value;
+        return this._amount * this._value;
     }
 }
