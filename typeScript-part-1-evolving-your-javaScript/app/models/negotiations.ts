6afafcee4e57c6c6b7cff3c002d38238ecff1e15
Lucas-Santos-dev <lucassantosrocha889@gmail.com>
2025-07-17 08:03:28 -0300

    Class 5

--- typeScript-part-1-evolving-your-javaScript/app/models/negotiations.ts
+++ typeScript-part-1-evolving-your-javaScript/app/models/negotiations.ts
@@ -1,12 +1,12 @@
 import { Negotiation } from "./negotiation.js";
 
 export class Negotiations {
-    private negotiations: Array<Negotiation> = [];
+    private negotiations: Negotiation[] = [];
 
     add(negotiation: Negotiation){
         this.negotiations.push(negotiation)
     }
-    list(): ReadonlyArray<Negotiation>{
+    list(): readonly Negotiation[] {
         return this.negotiations;
     }
 }
