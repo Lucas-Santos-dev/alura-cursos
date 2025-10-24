f4753c549213dfb0dcbd53532872c97069d431d5
Lucas-Santos-dev <lucassantosrocha889@gmail.com>
2025-10-24 00:15:39 -0300

    feat: add URL base

--- Angular-escreva-testes-end-to-end-com-playwright/3645-jornada-milhas-playwright-projeto-base/e2e/visitHomePage.spec.ts
+++ Angular-escreva-testes-end-to-end-com-playwright/3645-jornada-milhas-playwright-projeto-base/e2e/visitHomePage.spec.ts
@@ -2,7 +2,7 @@ import test, { expect } from "@playwright/test";
 
 test.describe("Home Page", () => {
     test("Must visit the home page", async({ page }) => {
-        await page.goto("http://localhost:4200/"); //action
+        await page.goto("/"); //action
 
         await expect(page).toHaveTitle("Jornada Milhas"); //asserção
 
