7d83f7a6cad3d31870e890ec15d19deb840888a8
Lucas-Santos-dev <lucassantosrocha889@gmail.com>
2025-10-23 23:58:23 -0300

    feat: add function capture passage titles

--- Angular-escreva-testes-end-to-end-com-playwright/3645-jornada-milhas-playwright-projeto-base/e2e/visitHomePage.spec.ts
+++ Angular-escreva-testes-end-to-end-com-playwright/3645-jornada-milhas-playwright-projeto-base/e2e/visitHomePage.spec.ts
@@ -2,7 +2,12 @@ import test, { expect } from "@playwright/test";
 
 test.describe("Home Page", () => {
     test("Must visit the home page", async({ page }) => {
-        await page.goto("http://localhost:4200/");
-        await expect(page).toHaveTitle("Jornada Milhas");
-    });
+        await page.goto("http://localhost:4200/"); //action
+
+        await expect(page).toHaveTitle("Jornada Milhas"); //asserção
+
+        const titleTickets = page.getByRole('heading', { name: 'Passagens' });
+
+        await expect(titleTickets).toBeVisible();
+    }); 
 });
