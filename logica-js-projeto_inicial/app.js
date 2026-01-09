0dbffc91786cb9d3bc6be63c6b63b37061768f33
Lucas-Santos-dev <lucassantosrocha889@gmail.com>
2026-01-09 01:02:24 -0300

    feat: update class 4

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -117,15 +117,17 @@
 //----- Aula 4 ----- //
 
 alert('Boas vindas ao jogo do número secreto');
-let secretNumber = 2;
 
 let chosenNumber;
 
 let attempts = 1;
 
+let secretNumber = parseInt(Math.random() * 100 + 1);
+console.log(secretNumber);
+
 //Enquanto chute não for igual ao número secreto
 while (chosenNumber != secretNumber) {
-    chosenNumber = prompt('Escolha um número entre 1 e 10');
+    chosenNumber = prompt('Escolha um número entre 1 e 100');
     // Se chosenNumber for igual ao número secreto
     if (chosenNumber == secretNumber) {
         break;
@@ -139,8 +141,33 @@ while (chosenNumber != secretNumber) {
     };
 }
 
-if (attempts > 1){
-    alert(`Isso aí! Você descobriu o número secreto "${secretNumber}" com ${attempts} tentativas`);
-} else {
-    alert(`Isso aí! Você descobriu o número secreto "${secretNumber}" com ${attempts} tentativa`);
-}
+let wordAttempts = attempts > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso aí! Você descobriu o número secreto "${secretNumber}" com ${attempts} ${wordAttempts}`);
+
+//----- Desafios aula 4 ----- //
+
+// Desafio 1: Crie um programa que utilize o console.log para exibir uma mensagem de boas-vindas.
+
+// Desafio 2: Crie uma variável chamada "nome" e atribua a ela o seu nome. Em seguida, utilize o console.log para exibir a mensagem "Olá, [seu nome]!" no console do navegador.
+
+// Desafio 3: Crie uma variável chamada "nome" e atribua a ela o seu nome. Em seguida, utilize o alert para exibir a mensagem "Olá, [seu nome]!" .
+
+// Desafio 4: Utilize o prompt e faça a seguinte pergunta: Qual a linguagem de programação que você mais gosta?. Em seguida, armazene a resposta em uma variável e mostre no console do navegador.
+
+// Desafio 5: Crie uma variável chamada "valor1" e outra chamada "valor2", atribuindo a elas valores numéricos de sua escolha. Em seguida, realize a soma desses dois valores e armazene o resultado em uma terceira variável chamada "resultado". Utilize o console.log para mostrar a mensagem "A soma de [valor1] e [valor2] é igual a [resultado]." no console.
+
+// Desafio 6: Crie uma variável chamada "valor1" e outra chamada "valor2", atribuindo a elas valores numéricos de sua escolha. Em seguida, realize a subtração desses dois valores e armazene o resultado em uma terceira variável chamada "resultado". Utilize o console.log para mostrar a mensagem "A diferença entre [valor1] e [valor2] é igual a [resultado]." no console.
+
+// Desafio 7: Peça ao usuário para inserir sua idade com prompt. Com base na idade inserida, utilize um if para verificar se a pessoa é maior ou menor de idade, exibindo uma mensagem apropriada no console.
+
+// Desafio 8: Crie uma variável "numero" e peça um valor com prompt verifique se é positivo, negativo ou zero. Use if-else para imprimir a respectiva mensagem.
+
+// Desafio 9: Use um loop while para imprimir os números de 1 a 10 no console.
+
+// Desafio 10: Crie uma variável "nota" e atribua um valor numérico a ela. Use if-else para determinar se a nota é maior ou igual a 7 e exiba "Aprovado" ou "Reprovado" no console.
+
+// Desafio 11: Use o Math.random para gerar qualquer número aleatório e exiba esse número no console.
+
+// Desafio 12: Use o Math.random para gerar um número inteiro entre 1 e 10 e exiba esse número no console.
+
+// Desafio 13: Use o Math.random para gerar um número inteiro entre 1 e 1000 e exiba esse número no console.
